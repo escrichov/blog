49ed1260638ea4bacc819f5a5753305477c68315
Carlos Escriche <escrichov@gmail.com>
2019-08-22 00:21:20 +0200

    Added custom about page, menu pages, github link, rss

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+from datetime import datetime
+
 AUTHOR = 'Carlos E'
 SITENAME = 'CE Blog'
 SITEURL = ''
@@ -13,27 +15,40 @@ TIMEZONE = 'Europe/Madrid'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('About', '/authors'),
-         ('Archive', '/archives'),)
+LINKS = (
+    ('Archive', '/archives'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('github', 'https://github.com/escrichov'),
+    ('rss', '/feeds/all.atom.xml'),
+)
 
-DEFAULT_PAGINATION = False
+COPYRIGHT_YEAR = datetime.now().year
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
 # Theme
 THEME = "pelican-themes/Flex"
+
+STATIC_PATHS = ['images', 'extra']
+
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+
+
+MENUITEMS = (('Archives', '/archives'), )
 
 # Plugings
 PLUGIN_PATHS = ['pelican-plugins']
